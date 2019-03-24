d0efd11c8c1c9a5f6cf8569948018551e8d163f6
yoopibit <yavorskyi.yurii@gmail.com>
2019-03-25 00:20:23 +0200

    Use hardware serial for CO2 sensor

--- sketch_mar19a.ino
+++ sketch_mar19a.ino
@@ -5,7 +5,7 @@
 #define DHTTYPE DHT22
 
 
-SoftwareSerial mySerial(A0, A1); // A0 - к TX сенсора, A1 - к RX
+//SoftwareSerial mySerial(A0, A1); // A0 - к TX сенсора, A1 - к RX
 DHT dht(DHTPIN, DHT22);
 
 SoftwareSerial SerialBt(3, 4); // 3 - TX 4 - RX
@@ -16,47 +16,9 @@ unsigned char response[9];
 
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(9600);
+//  mySerial.begin(9600);
   dht.begin();
   SerialBt.begin(9600);
-}
-
-void old_functiounality()
-{
-  mySerial.write(cmd, 9);
-  memset(response, 0, 9);
-  mySerial.readBytes((char*)response, 9);
-  int i;
-  byte crc = 0;
-  for (i = 1; i < 8; i++) crc+=response[i];
-  crc = 255 - crc;
-  crc++;
-
-  if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
-    Serial.println("CRC error new: " + String(crc) + " / "+ String(response[8]));
-  } 
-  else {
-    unsigned int responseHigh = (unsigned int) response[2];
-    unsigned int responseLow = (unsigned int) response[3];
-    unsigned int ppm = (256*responseHigh) + responseLow;
-    Serial.println(ppm);
-  }
-
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  if (isnan(t) || isnan(h)) {
-    Serial.println("Failed to read from DHT");
-  }
-  else {
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.println(" *C");
-  }
-  delay(10000);
 }
 
 bool is_get_arrived()
@@ -71,8 +33,8 @@ bool is_get_arrived()
 
   if (received_str.length() != 0)
   {
-    Serial.print("Received command: ");
-    Serial.write(received_str.c_str());
+    //Serial.print("Received command: ");
+    //Serial.write(received_str.c_str());
 
     if (received_str[0] == 'G')
       return true;
@@ -87,9 +49,9 @@ void loop()
     return;
   }
 
-  mySerial.write(cmd, 9);
+  Serial.write(cmd, 9);
   memset(response, 0, 9);
-  mySerial.readBytes((char*)response, 9);
+  Serial.readBytes((char*)response, 9);
   int i;
   byte crc = 0;
   for (i = 1; i < 8; i++) crc+=response[i];
@@ -97,13 +59,13 @@ void loop()
   crc++;
 
   if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
-    Serial.println("CRC error new: " + String(crc) + " / "+ String(response[8]));
+    SerialBt.println("CRC error new: " + String(crc) + " / "+ String(response[8]));
   } 
   else {
     unsigned int responseHigh = (unsigned int) response[2];
     unsigned int responseLow = (unsigned int) response[3];
     unsigned int ppm = (256*responseHigh) + responseLow;
-    Serial.println(ppm);
+   // Serial.println(ppm);
     SerialBt.print("CO2: ");
     SerialBt.println(ppm);
 
@@ -113,16 +75,16 @@ void loop()
   float t = dht.readTemperature();
 
   if (isnan(t) || isnan(h)) {
-    Serial.println("Failed to read from DHT");
+  //  Serial.println("Failed to read from DHT");
     SerialBt.println("Failed to read from DHT");
   }
   else {
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.println(" *C");
+//    Serial.print("Humidity: ");
+//    Serial.print(h);
+//    Serial.print(" %\t");
+//    Serial.print("Temperature: ");
+//    Serial.print(t);
+//    Serial.println(" *C");
 
 
     SerialBt.print("Humidity: ");
@@ -133,6 +95,6 @@ void loop()
     SerialBt.println(" *C");
     SerialBt.println("");
   }
-  delay(10000);
+  //delay(10000);
 }
 
