d02f5bc1671e608679e9e96c443248fefd2e27f8
yoopibit <yavorskyi.yurii@gmail.com>
2019-03-31 01:26:32 +0200

    add suport the mh library

--- sketch_mar19a.ino
+++ sketch_mar19a.ino
@@ -1,4 +1,5 @@
 #include <mhz19b.h>
+
 #include <SoftwareSerial.h>
 SoftwareSerial co2Serial(A0, A1); // A0 - к TX сенсора, A1 - к RX
 unsigned long startTime = millis();
@@ -7,7 +8,9 @@ Mhz19b co2(co2Serial);
 void setup() {
   Serial.begin(9600);
   co2Serial.begin(9600);
-  pinMode(9, INPUT);
+  delay(1000);
+  int ret = co2.set_auto_calibrate(false);
+  Serial.println(co2.get_last_log() + String("Ret from auto_calibrate = ") + String(ret));
 }
  
 void loop() {
@@ -15,9 +18,10 @@ void loop() {
   Serial.print("Time from start: ");
   Serial.print((millis() - startTime) / 1000);
   Serial.println(" s");
-  int ppm_uart = co2.getCO2uart();
+  int ppm_uart = co2.get_co2_uart();
   Serial.println(co2.get_last_log());
-  
+  Serial.println("GET from function: " + String(ppm_uart));
+
   delay(10000);
 }
 
