93f19a663688e6eade3382159eb78c1df5bf9b2a
yoopibit <yavorskyi.yurii@gmail.com>
2019-04-07 22:50:41 +0300

    add battery level library

--- sketch_mar19a.ino
+++ sketch_mar19a.ino
@@ -1,9 +1,12 @@
+
+#include <lipo.h>
 #include <mhz19b.h>
 #include <SoftwareSerial.h>;
 #include "DHT.h"
 
 #define DHTPIN 2
 DHT dht(DHTPIN, DHT22);
+Lipo lipo(14, 21, A0, 33);
 
 SoftwareSerial SerialBt(3, 4); // 3 - TX 4 - RX
 Mhz19b co2(Serial);
@@ -39,12 +42,12 @@ void loop()
     return;
   }
 
-  int ppm_uart = co2.get_co2_uart();
-//  SerialBt.println("GET from function: " + String(ppm_uart));
+  int battery_level = lipo.get_level();
 
-  
-//  SerialBt.print("CO2: ");
-  SerialBt.println(co2.get_last_log());
+  int ppm_uart = co2.get_co2_uart();
+
+  SerialBt.println(co2.get_log_debug());
+  SerialBt.println(lipo.get_log_debug());
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -57,6 +60,9 @@ void loop()
     SerialBt.print(h);
     SerialBt.print(" %, Temperature: ");
     SerialBt.print(t);
+    SerialBt.print(" Voltage: ");
+    SerialBt.print(battery_level);
+
     SerialBt.print("C\n");
   }
 }
